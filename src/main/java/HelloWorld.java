ccfd702f16f4f571a917513333470582837d9725
Carey Norslien <careynorslien@gmail.com>
2019-08-06 19:36:16 -0500

    Write more JSON files using Lists and Maps.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HelloWorld {
 
@@ -12,7 +16,16 @@ public class HelloWorld {
         MyValue m = new MyValue();
         m.name = "Alice";
         m.age = 26;
-
         mapper.writeValue(new File("C:\\Users\\Carey\\Desktop\\data.json"), m);
+
+        List<String> names = new ArrayList<String>();
+        names.add("Bob");
+        names.add("Cindy");
+        mapper.writeValue(new File("C:\\Users\\Carey\\Desktop\\data1.json"), names);
+
+        Map<String, Integer> info = new HashMap<String, Integer>();
+        info.put("Sally", 30);
+        info.put("John", 31);
+        mapper.writeValue(new File("C:\\Users\\Carey\\Desktop\\data2.json"), info);
     }
 }
